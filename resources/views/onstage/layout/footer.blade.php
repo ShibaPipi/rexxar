e1213458add24cfb3a06825a7675805b77d5da77
sun.yaopeng <906262260@qq.com>
2020-06-19 16:40:59 +0800

    ICP 备案号上传

--- resources/views/onstage/layout/footer.blade.php
+++ resources/views/onstage/layout/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="blog-footer">
     <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-    <p>备案号：<a href="http://www.beian.miit.gov.cn">津ICP备19011078号</a></p>
+    <p>备案号：<a href="http://www.beian.miit.gov.cn" target="_blank">津ICP备19011078号</a></p>
     <p>
         <a class="cd-top cd-is-visible" href="#">Back to top</a>
     </p>
