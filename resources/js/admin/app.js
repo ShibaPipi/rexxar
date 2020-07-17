5d4446fc50f04b4249a039c05a11519acd326853
sun.yaopeng <906262260@qq.com>
2020-07-17 14:44:59 +0800

    后台重构搭建

--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import less from 'less'
-import VueParticles from 'vue-particles'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -9,7 +8,6 @@ import ELEMENT from 'element-ui'
 
 Vue.use(ELEMENT);
 Vue.use(less);
-Vue.use(VueParticles);
 
 /**
  * 不显示生产模式的消息
