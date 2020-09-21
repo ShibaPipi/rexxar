bdf6a5718dfe3833c2d4976508725f1a17f9560c
sun.yaopeng <906262260@qq.com>
2020-09-21 15:33:41 +0800

    项目优化

--- app/Http/Controllers/Api/V2/Admin/AdminUserController.php
+++ app/Http/Controllers/Api/V2/Admin/AdminUserController.php
@@ -25,7 +25,9 @@ class AdminUserController extends Controller
 
     public function info()
     {
-        return api()->success(auth()->user());
+        $user = '{"roles":["admin"],"introduction":"I am a super administrator","avatar":"https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif","name":"Super Admin"}';
+        return api_response()->success(json_decode($user));
+//        return api_response()->success(auth()->user());
     }
 
     public function login(Request $request)
